1a5aa9cb219ad0a8806855ac8c6a58cfbe3728e3
Emilio Cano Renteria <emiliocanor@princeton.edu>
2022-10-31 00:36:57 -0400

    rip validate.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-
-
 from sys import path
 import os
-
 path.append('src')  #go to src directory to import
 from flask import Flask, render_template, redirect
 from CASClient import CASClient
 import secrets
-import validation
-<<<<<<< Updated upstream
+import posts
 import profile
 
 CLUB_SOCC = 1
-=======
-import subprocess
-import posts
->>>>>>> Stashed changes
+
 
 # app info
 app = Flask(__name__)
@@ -39,10 +32,6 @@ def application():
     else:
         return render_template('invalid.html', CASValue = netid)
 
-@app.route('/unvalidated')
-def unvalidated():
-    return render_template('invalid.html', CASValue = netid, validation = is_in_club)
-
 @app.route('/members')
 def members():
     members = [{"name": "john", "year": 2020, "position": "mid"}, {"name": "frank", "year": 2020, "position": "striker"}, {"name": "mollie", "year": 2020, "position": "striker"}, {"name": "john", "year": 2020, "position": "mid"}, {"name": "frank", "year": 2020, "position": "striker"}, {"name": "mollie", "year": 2020, "position": "striker"}]
@@ -53,7 +42,6 @@ def announcements():
     print(post_values)
     # print(posts)
     return render_template('announcements.html', posts = post_values)
-
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5555, debug=True)
