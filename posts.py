1b76768c40ccdcbbc5ac7abf4d8c849dd31c76cc
Emilio Cano Renteria <emiliocanor@princeton.edu>
2022-11-15 00:41:04 -0500

    admin back end

--- posts.py
+++ posts.py
@@ -5,6 +5,7 @@ import database
 import profile
 from datetime import datetime
 
+
 class Post:
 
     def __init__(self, db_row):
@@ -14,6 +15,7 @@ class Post:
         self._title = db_row.title
         self._description = db_row.description
         self._timestamp = db_row.timestamp
+        self._status = db_row.status
 
     def get_post_id(self):
         return self._post_id
@@ -32,10 +34,13 @@ class Post:
 
     def get_timestamp(self):
         return self._timestamp
+    
+    def get_status(self):
+        return self._status
 
-# -----------------------------------------------------------------------
+#-----------------------------------------------------------------------
 
-def make_posts(post_title,post_description ):
+def make_request(post_title, post_description):
     DATABASE_URL = os.getenv('DB_URL')
     if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
@@ -46,12 +51,12 @@ def make_posts(post_title,post_description ):
                                 title=post_title,
                                 description=post_description,
                                 club_image_url="https://www.princeton.edu/~clubsocc/img/team_main.jpeg",
-                                timestamp=datetime.now())
+                                timestamp=datetime.now(),
+                                status = 0)
             session.add(post1)
             session.commit()
             session.refresh(post1)
             return post1.post_id
-            print("added to database")
     finally:
         engine.dispose()
 
@@ -63,7 +68,7 @@ def get_posts():
     engine = sqlalchemy.create_engine(DATABASE_URL)
     try:
         with sqlalchemy.orm.Session(engine) as session:
-            query = session.query(database.Posts)
+            query = session.query(database.Posts).filter(database.Posts.status == 1)
             # print(query)
             table = query.all()
             list = []
@@ -95,13 +100,71 @@ def add_image(post_id, post_img_url):
     finally:
         engine.dispose()
 
+def get_requests():
+    DATABASE_URL = os.getenv('DB_URL')
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    engine = sqlalchemy.create_engine(DATABASE_URL)
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            query = session.query(database.Posts).filter(database.Posts.status == 0)
+            # print(query)
+            table = query.all()
+            list = []
+            for row in table:
+                # print(row)
+                post = Post(row)
+                user = profile.get_profile_from_id(post._creator_id)
+                list.append({"post": post, "user": user})
+            session.commit()
+            return list
+    finally:
+        engine.dispose()
+
+def approve_request(post_id):
+    DATABASE_URL = os.getenv('DB_URL')
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    engine = sqlalchemy.create_engine(DATABASE_URL)
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            # print(post_netid)
+            print(post_id)
+            query = session.query(database.Posts).filter(
+                    database.Posts.post_id == post_id)
+            row = query.one()
+            row.status = 1
+            session.commit()
+    finally:
+        engine.dispose()
+    
+def reject_request(post_id):
+    DATABASE_URL = os.getenv('DB_URL')
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    delete_post(post_id)
+
+def delete_post(post_id):
+    DATABASE_URL = os.getenv('DB_URL')
+    engine = sqlalchemy.create_engine(DATABASE_URL)
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            # print(post_netid)
+            print(post_id)
+            stmt = sqlalchemy.delete(database.Posts).where((database.Posts.post_id == post_id))
+            session.execute(stmt)
+            session.commit()
+            print("deleted")
+    finally:
+        engine.dispose()
+
+
 # -----------------------------------------------------------------------
 
 # For testing:
 def _test():
     add_image(1, "https://res.cloudinary.com/clubnet/image/upload/v1668410349/gfcmsvzylqxuumuqpmhy.png")
     print(get_posts())
-
 
 if __name__ == '__main__':
     _test()
